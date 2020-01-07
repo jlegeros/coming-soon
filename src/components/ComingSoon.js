5f1ef6032b3efa498c7107dfe216670d3d37449f
jlegeros <jlegeros@gmail.com>
2020-01-07 12:46:33 -0500

    created logo, title, and description

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,5 +1,10 @@
 import React, { Component } from 'react';
 import Countdown from './Countdown';
+import { Logo } from './Logo';
+import { Title } from './Title';
+import { Description } from './Description';
+
+import logo from "../images/gear.svg"
 
 import '../styles/ComingSoon.css';
 
@@ -7,16 +12,29 @@ class ComingSoon extends Component {
   state = {
     countdown: {
       futureDate: "2020-12-31 00:00:00"
+    },
+    logo: {
+      alt: "spinning gear",
+      src: logo
+    },
+    title: {
+      text: 'Coming soon!'
+    },
+    description: {
+      text: "Some description text that I don't want to copy at this time, will update later, bye Felicia!"
     }
   }
 
   render() {
     const {
-      countdown
+      countdown, logo, title, description
     } = this.state;
     return (
       <div className="background">
         <Countdown futureDate={countdown.futureDate}/>
+        <Logo alt={ logo.alt } src={ logo.src }/>
+        <Title text={ title.text } />
+        <Description text={ description.text } />
       </div>
     )};
 }
---
+++ src/components/Title.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import "../styles/Title.css";
+
+export const Title = ({ text }) => {
+  return (
+    <h1 className="title">{text}</h1>
+  )
+}
