66163cbc9aaad1d7cb7949e1643c74d042541614
jlegeros <jlegeros@gmail.com>
2020-01-07 11:16:44 -0500

    created and styled countdown component

--- src/components/ComingSoon.js
+++ src/components/ComingSoon.js
@@ -1,12 +1,24 @@
-import React from 'react';
+import React, { Component } from 'react';
+import Countdown from './Countdown';
+
 import '../styles/ComingSoon.css';
 
-function ComingSoon() {
-  return (
-    <div className="background">
-      <h1>Coming Soon</h1>
-    </div>
-  );
+class ComingSoon extends Component {
+  state = {
+    countdown: {
+      futureDate: "2020-12-31 00:00:00"
+    }
+  }
+
+  render() {
+    const {
+      countdown
+    } = this.state;
+    return (
+      <div className="background">
+        <Countdown futureDate={countdown.futureDate}/>
+      </div>
+    )};
 }
 
 export default ComingSoon;
